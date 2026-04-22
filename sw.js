2d612473bd08d1fdd9e51a232be21660ac5699dc
WenXiangStudio <yiaotian43@gmail.com>
2026-04-22 23:05:51 +0800

    perf: speed up phonetic rendering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wordkiller-v3';
+const CACHE_NAME = 'wordkiller-v4';
 const APP_SHELL = [
   new URL('./', self.location).toString(),
   new URL('index.html', self.location).toString(),
