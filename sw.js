0b239a9e26673ef69cbb5deeadad026f0d34b9cc
WenXiangStudio <yiaotian43@gmail.com>
2026-04-23 01:26:20 +0800

    feat: show version and lock wrong choices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wordkiller-v8';
+const CACHE_NAME = 'wordkiller-v9';
 const APP_SHELL = [
   new URL('./', self.location).toString(),
   new URL('index.html', self.location).toString(),
