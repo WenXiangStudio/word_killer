ffcacd3d54fac2a7fb5dd491d3a4cd31da19ed5e
WenXiangStudio <yiaotian43@gmail.com>
2026-04-22 23:55:59 +0800

    fix: add pronunciation player fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wordkiller-v5';
+const CACHE_NAME = 'wordkiller-v6';
 const APP_SHELL = [
   new URL('./', self.location).toString(),
   new URL('index.html', self.location).toString(),
