90b762b855140870d7622d0b649d3b50ba190c5e
WenXiangStudio <yiaotian43@gmail.com>
2026-04-27 23:02:35 +0800

    feat: add app-style home navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wordkiller-v16';
+const CACHE_NAME = 'wordkiller-v17';
 const APP_SHELL = [
   new URL('./', self.location).toString(),
   new URL('index.html', self.location).toString(),
