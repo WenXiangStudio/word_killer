90d3c3e320693b9b5974590c2cc7ffacc770de00
WenXiangStudio <yiaotian43@gmail.com>
2026-04-27 22:18:00 +0800

    fix: improve learning flow and book loading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wordkiller-v14';
+const CACHE_NAME = 'wordkiller-v15';
 const APP_SHELL = [
   new URL('./', self.location).toString(),
   new URL('index.html', self.location).toString(),
